8adf80f8bf551e9953c1d7ea06188ed279aaec7d
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2022-12-10 20:44:37 +0700

    Creating signup page and finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </React.Fragment>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='max-w-md m-auto h-max absolute top-0 bottom-0 left-0 right-0 flex flex-col text-center px-3'>
+      <Link to='/login' className='text-lg border border-solid border-black rounded-full py-2'>Login</Link>
+      <Link to='/signup' className='py-2 text-lg bg-black text-white rounded-full mt-3'>Sign Up</Link>
+    </div>
   )
 }
 
